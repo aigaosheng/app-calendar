3558db0577137c6e3605cab07a3c9701e39f1f05
gs <goseng123@gmail.com>
2020-07-31 23:53:47 +0800

    a

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
+// home.module.ts
+
 import { Component } from '@angular/core';
+import { CalendarComponentOptions } from 'ion2-calendar'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  dateMulti: string[];//= ['2018-01-01', '2018-01-02', '2018-01-05'];
+  type: 'string';
+  
+  optionsMulti: CalendarComponentOptions = {
+    from: new Date(2020,0,1),
+    pickMode: 'multi'
+  };
 
-  constructor() {}
+  onChange($event) {
+    console.log($event);
+  }
 
+  constructor() { }
 }
